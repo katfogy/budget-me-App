600c51d605c49d0650aa17b2d30ff51cc1056d00
katfogy <katfogy@gmail.com>
2023-10-26 13:51:53 +0100

    category testing updated

--- spec/features/category_new_spec.rb
+++ spec/features/category_new_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'categories/new', type: :view do
   before(:each) do
     assign(:category, Category.new(
-                        name: 'MyString',
-                        icon: 'MyString'
+                        name: 'Foga',
+                        icon: 'Foga'
                       ))
   end
 
